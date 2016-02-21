2efe9a66acf4cfa79586caed2aabe2bb0a29c5c8
Pavel <pavlinter@gmail.com>
2016-02-21 16:42:03 +0200

    fix timezone

--- config/db-local.php
+++ config/db-local.php
@@ -6,4 +6,9 @@ return [
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
+    'on afterOpen' => function ($event) {
+        /* @var $sender \app\components\Connection */
+        $sender = $event->sender;
+        //$sender->pdo->exec('SET time_zone = "' . Yii::$app->timeZone . '"');
+    },
 ];
--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ return [
     'password' => '',
     'charset' => 'utf8',
     'on afterOpen' => function ($event) {
-        /* @var $sender \yii\db\Connection */
+        /* @var $sender \app\components\Connection */
         $sender = $event->sender;
         $sender->pdo->exec('SET time_zone = "' . Yii::$app->timeZone . '"');
     },
