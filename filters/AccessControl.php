afa00335dcb4e40aa60805007a8080d800e25d21
Pavel <pavlinter@gmail.com>
2016-03-04 22:48:53 +0200

    ...

---
+++ filters/AccessControl.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\behaviors;
+
+use app\components\User;
+use Yii;
+use yii\di\Instance;
+
+/**
+ * Class AccessControl
+ */
+class AccessControl extends \yii\filters\AccessControl
+{
+    /**
+     * Initializes the [[rules]] array by instantiating rule objects from configurations.
+     */
+    public function init()
+    {
+        parent::init();
+        $this->user = Instance::ensure($this->user, User::className());
+        foreach ($this->rules as $i => $rule) {
+            if (is_array($rule)) {
+                $this->rules[$i] = Yii::createObject(array_merge($this->ruleConfig, $rule));
+            }
+        }
+    }
+}
---
+++ filters/VerbFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\behaviors;
+
+use Yii;
+
+/**
+ * Class VerbFilter
+ */
+class VerbFilter extends \yii\filters\VerbFilter
+{
+
+}
