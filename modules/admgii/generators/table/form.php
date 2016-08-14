a2b6597e03e9b3d0e808973da13f9d99a388b6f3
Pavel <pavlinter@gmail.com>
2016-08-14 17:33:55 +0300

    re

--- modules/admgii/generators/table/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this yii\web\View */
-/* @var $form yii\widgets\ActiveForm */
-/* @var $generator app\modules\admgii\generators\table\Generator */
-
-echo $form->field($generator, 'tableName');
-echo $form->field($generator, 'isLang')->checkbox();
-echo $form->field($generator, 'db');
-echo $form->field($generator, 'useTablePrefix')->checkbox();
