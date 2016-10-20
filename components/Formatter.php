f953583aae7288e44114ff794479698f62633eca
Pavel <pavlinter@gmail.com>
2016-10-20 12:06:39 +0300

    add asPrice

--- components/Formatter.php
+++ components/Formatter.php
@@ -9,5 +9,15 @@ use Yii;
  */
 class Formatter extends \yii\i18n\Formatter
 {
-
+    /**
+     * @param $price
+     * @param int|null $decimal
+     * @param array $options
+     * @param array $textOptions
+     * @return string
+     */
+    public function asPrice($price, $decimal = 2, $options = [], $textOptions = [])
+    {
+        return "€" . $this->asDecimal($price, $decimal, $options, $textOptions);
+    }
 }
