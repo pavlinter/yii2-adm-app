cb45dfd28f35e18298a2b0b7fdfc288f4e84656a
Pavel <pavlinter@gmail.com>
2016-10-29 11:07:33 +0300

    changed model structure

---
+++ views/site/resetPassword.php
@@ -0,0 +1,28 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \app\models\form\ResetPasswordForm */
+
+$this->title = Yii::t("app/passwordReset", "Reset password", ['dot' => false]);
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-reset-password">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p><?= Yii::t("app/passwordReset", "Please choose your new password:") ?></p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+                <?= $form->field($model, 'password')->passwordInput() ?>
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t("app/passwordReset", "Save", ['dot' => false]), ['class' => 'btn btn-primary']) ?>
+                    <?= Yii::t("app/passwordReset", "Save", ['dot' => '.']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
---
+++ views/site/signup.php
@@ -0,0 +1,36 @@
+<?php
+use yii\captcha\Captcha;
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \app\models\form\SignupForm */
+
+$this->title = Yii::t("app/signup", "Registration", ['dot' => false]);
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-signup">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p><?= Yii::t("app/signup", "Please fill out the following fields to signup:") ?></p>
+
+    <?= \app\widgets\Alert::widget() ?>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                <?= $form->field($model, 'username') ?>
+                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::className()) ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t("app/signup", "Signup", ['dot' => false]), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                    <?= Yii::t("app/signup", "Signup", ['dot' => '.']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
