31da01913d00377ff1c0aa8b4c2667465907a4d1
Pavel <pavlinter@gmail.com>
2017-01-27 20:32:35 +0200

    version 0.0.1

--- views/admpages/default/main.php
+++ views/admpages/default/main.php
@@ -1,9 +1,11 @@
 <?php
 
+use app\helpers\Url;
+
 /* @var $this yii\web\View */
 /* @var $model app\core\admpages\models\Page */
 
-Yii::$app->params['html.canonical'] = Yii::$app->homeUrl;
+Yii::$app->params['html.canonical'] = Url::to('', true);
 ?>
 <div class="main-page">
     <h1><?= $model->title ?></h1>
